3b8808e8c176752f5f079e54f533a6a6bd0464f1
Samsyne <samsonyekini@gmail.com>
2023-06-19 10:25:26 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,8 +8,12 @@ int main(void)
 int i;
 char letter;
 for (i = 0; i < 10; i++)
-putchar(( num % 10) + '0');
-for (letter = 'a'; letter <= 'f';letter++);
+putchar((i % 10) + '0');
+for
+(letter = 'a';
+letter <= 'f';
+letter++);
+putchar(letter);
 putchar('\n');
 return (0);
 }
