690f50db7481bb3651e7c450a86cb226a0881f79
Samsyne <samsonyekini@gmail.com>
2023-07-08 15:29:47 -0700

    library is the fuel tank of knowledge

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase letters
+ * @c: char to check
+ *
+ * Return: 1if c is uppercase or 0 otherwise
+ */
+
+int _isupper(int c)
+{
+        if (c >= 'A' && c <= 'Z')
+        return (1);
+        else
+        return (0);
+}
