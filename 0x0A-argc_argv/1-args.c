fca83873d1c58af6a660009bf5fe516eed7ddce0
Samsyne <samsonyekini@gmail.com>
2023-07-10 05:15:16 -0700

    A program that counts apart from program name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program to print a no of arguments
+ * @argc: no of argument count
+ * @argv: an array of command line arguement
+ * Return: 0 is success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("argc = %d\n", argc - 1);
+	return (0);
+}
