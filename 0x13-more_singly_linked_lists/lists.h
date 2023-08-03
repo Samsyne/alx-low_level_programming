1e55b3ad5d859abca36794a9627a74f5f8ccbf24
Samsyne <samonyekini@gmail.com>
2023-08-02 17:54:32 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_s - self refrencial struct
+ * for creating nodes of singly list
+ *
+ * @n: integer data
+ * @next: self refrencial pointer
+ * pointer to the next node of the list
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}listint_t;
+
+
+size_t print_listint(const listint_t *h);
+
+#endif
